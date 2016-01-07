4a649f4cc24c33773a2ad52e0fba73ac52ecd781
polargull <fuwei04ruanjian@gmail.com>
2016-01-07 13:49:45 +0800

    debug log recorde

--- polarbear/src/main/java/com/polarbear/web/login/LoginController.java
+++ polarbear/src/main/java/com/polarbear/web/login/LoginController.java
@@ -1,11 +1,14 @@
 package com.polarbear.web.login;
 
-import static com.polarbear.util.Constants.ResultState.*;
+import static com.polarbear.util.Constants.ResultState.LOGIN_NAME_PWD_ERR;
+import static com.polarbear.util.Constants.ResultState.SUCCESS;
 
 import javax.security.auth.login.LoginException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +25,7 @@ import com.polarbear.util.cookie.UserCookieUtil;
 
 @Controller
 public class LoginController {
+    private Log log = LogFactory.getLog(LoginController.class);
     @Autowired(required = false)
     private LoginService loginService;
 
@@ -29,6 +33,7 @@ public class LoginController {
     @ResponseBody
     public Object login(HttpServletResponse response, HttpServletRequest request, @RequestParam("uname") String uname, @RequestParam("password") String password) {
         try {
+            log.debug("uname:" + uname + ",password:" + password);
             validate(uname, password);
             LoginData loginData = loginService.login(uname, password);
             UserCookieUtil.saveUserCookie(loginData.getUser(), request, response, 0);
