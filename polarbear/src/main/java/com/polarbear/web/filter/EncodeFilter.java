9614efb7e6313aabe3d229a54fdd81694e9079e2
polargull <fuwei04ruanjian@gmail.com>
2016-01-07 13:49:05 +0800

    use custorm encode filter

--- polarbear/src/main/java/com/polarbear/web/filter/EncodeFilter.java
+++ polarbear/src/main/java/com/polarbear/web/filter/EncodeFilter.java
@@ -1,4 +1,4 @@
-package com.polarbear.web.filter.monitor;
+package com.polarbear.web.filter;
 
 import java.io.IOException;
 
@@ -8,13 +8,8 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-public class PerformanceMonitorFilter implements Filter {
-    private static Log log = LogFactory.getLog(PerformanceMonitorFilter.class);
+public class EncodeFilter implements Filter {
 
     @Override
     public void destroy() {
@@ -23,10 +18,9 @@ public class PerformanceMonitorFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        long beginTime = System.currentTimeMillis();
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json;charset=UTF-8");
         chain.doFilter(request, response);
-        log.info("url:" + httpRequest.getRequestURI() + ", time consuming:" + (System.currentTimeMillis() - beginTime));
     }
 
     @Override
