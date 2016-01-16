8991aebe7bed30854d64acdbbf969b646d8923df
polargull <fuwei04ruanjian@gmail.com>
2016-01-16 13:51:15 +0800

    add RemoveShopcartProductCompoment

--- polarbear/src/main/java/com/polarbear/service/shopcart/RemoveShopcartProductComponent.java
+++ polarbear/src/main/java/com/polarbear/service/shopcart/RemoveShopcartProductComponent.java
@@ -8,17 +8,24 @@ import com.polarbear.dao.DaoException;
 import com.polarbear.domain.Product;
 import com.polarbear.domain.Shopcart;
 import com.polarbear.domain.ShopcartDetail;
+import com.polarbear.domain.User;
+import com.polarbear.util.factory.CurrentThreadUserFactory;
 
 @Component
 public class RemoveShopcartProductComponent {
     @Autowired
     BaseDao<ShopcartDetail> shopcartDetailDao;
-    
-    public void removeProductFromShopcart(Shopcart shopcart, Product product) throws DaoException {
-        ShopcartDetail sd = shopcartDetailDao.findByNamedQueryObject("queryByShopcartAndProduct", shopcart, product);
-        if (sd != null) {
-            shopcartDetailDao.delete(sd);
-        }
+    @Autowired
+    BaseDao<Shopcart> shopcartDao;
+    @Autowired
+    BaseDao<Product> productDao;
+
+    public void removeProductFromShopcart(long pid) throws DaoException {
+        User u = CurrentThreadUserFactory.getUser();
+        Product p = productDao.findById(Product.class, pid);
+        Shopcart shopcart = shopcartDao.findByNamedQueryObject("queryUserId", u);
+        ShopcartDetail shopcartDetail = shopcartDetailDao.findByNamedQueryObject("queryByShopcartAndProduct", shopcart, p);
+        shopcartDetailDao.delete(shopcartDetail);
     }
-    
+
 }
