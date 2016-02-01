1172227e2704577a073dfeca88cd5163a84f48fd
polargull <fuwei04ruanjian@gmail.com>
2016-02-01 15:52:50 +0800

    back console user\product\order list

---
+++ polarbear/WebRoot/js/biz/main.portal.js
@@ -0,0 +1,28 @@
+function initPortal () {
+    $('#pp').portal({
+        border:false,
+        fit:true
+    });
+    add();
+}
+function add() {
+    for(var i=0; i<3; i++) {
+        var p = $('<div/>').appendTo('body');
+        p.panel({
+            title:'Title'+i,
+            content:'<div style="padding:5px;">Content'+(i+1)+'</div>',
+            height:100,
+            closable:true,
+            collapsible:true
+        });
+        $('#pp').portal('add', {
+            panel:p,
+            columnIndex:i
+        });
+    }
+    $('#pp').portal('resize');
+}
+function remove() {
+    $('#pp').portal('remove',$('#pgrid'));
+    $('#pp').portal('resize');
+}
---
+++ polarbear/WebRoot/js/biz/main.tab.js
@@ -0,0 +1,44 @@
+function initTabs () {
+    $('#tt').tabs({
+        border:false,
+        onSelect:function(title){
+            updateTab(title);
+        }
+    })
+}
+function updateTab (title) {
+    if (title == '首页') {
+        return;
+    }
+    var current_tab = $('#tt').tabs('getSelected');    
+    $('#tt').tabs('update',{
+         tab:current_tab,
+         options : {  
+              // content : 'Introduction to language',  
+              href:nav(title)
+          //或者 href : '';  
+         }  
+    }); 
+}
+function nav (title) {
+  if (title == '用户管理') {
+      return 'user_manager.html';
+  } else if (title == '商品管理') {
+      return 'product_manager.html';
+  } else if (title == '订单管理') {
+    return 'order_manager.html';
+  }
+}
+function addTab(title) {        
+    $('#tt').tabs('add',{
+        title:title,
+        content:'Tab Body',
+        closable:true,
+        tools:[{
+            iconCls:'icon-mini-refresh',
+            handler:function(){
+                alert('refresh');
+            }
+        }]
+    });
+}
