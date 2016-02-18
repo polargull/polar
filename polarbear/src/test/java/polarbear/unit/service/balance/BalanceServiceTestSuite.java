ab85fdb357c35360aa11c2b217c8bb7b15979295
polargull <fuwei04ruanjian@gmail.com>
2016-02-18 15:54:07 +0800

    balance test suite

---
+++ polarbear/src/test/java/polarbear/unit/service/balance/BalanceServiceTestSuite.java
@@ -0,0 +1,11 @@
+package polarbear.unit.service.balance;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( { BalanceDataTOTest.class })
+public class BalanceServiceTestSuite {
+    // the class remains empty,
+    // used only as a holder for the above annotations
+}
