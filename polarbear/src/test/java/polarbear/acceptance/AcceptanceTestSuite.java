7a5c93c425038e1cda75c3e7438b299ef5c3a684
polargull <fuwei04ruanjian@gmail.com>
2016-02-21 18:02:15 +0800

    balance acceptance test

--- polarbear/src/test/java/polarbear/acceptance/AcceptanceTestSuite.java
+++ polarbear/src/test/java/polarbear/acceptance/AcceptanceTestSuite.java
@@ -4,13 +4,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import polarbear.acceptance.back.AcceptanceBackTestSuite;
+import polarbear.acceptance.balance.BalanceTest;
 import polarbear.acceptance.login.LoginTest;
 import polarbear.acceptance.product.ProductTestSuite;
 import polarbear.acceptance.regist.RegistTestSuite;
 import polarbear.acceptance.shopcart.ShopcartTestSuite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { LoginTest.class, RegistTestSuite.class, ShopcartTestSuite.class, ProductTestSuite.class, AcceptanceBackTestSuite.class })
+@Suite.SuiteClasses( { LoginTest.class, RegistTestSuite.class, ShopcartTestSuite.class, ProductTestSuite.class, AcceptanceBackTestSuite.class, BalanceTest.class })
 public class AcceptanceTestSuite {
 
 }
--- polarbear/src/test/java/polarbear/acceptance/balance/BalanceTest.java
+++ polarbear/src/test/java/polarbear/acceptance/balance/BalanceTest.java
@@ -24,10 +24,9 @@ public class BalanceTest {
     public void shouldReturnCorrectBalanceDataWhenSelectNumEachIs1TwoProduct() {
         final Product p1 = ProductAcceptanceTestDataFactory.createProduct1();
         final Product p2 = ProductAcceptanceTestDataFactory.createProduct2();
-        final Product salePriceIs6P3 = ProductAcceptanceTestDataFactory.createSalePrice6Product3();
-        String pids = p1.getId() + "," + p2.getId() + "," + salePriceIs6P3.getId();
+        String pids = p1.getId() + "," + p2.getId();
         String nums = "1,1";
-        final double LOGISTIC_PRICE = 10d;
+        final double LOGISTIC_PRICE_0 = 0d;
         anRequest(BALANCE_URL)
                 .withCookie(LoginController.USER_LOGIN_COOKIE, "MToxNDUxOTgyNjQzNTQ0OjM1ZWJhMDVjMjY5NTMxNjc5OWM1YmYwM2Q0YTE5N2M3")
                 .addParams("pids", pids)
@@ -37,9 +36,9 @@ public class BalanceTest {
                         assertThat(resultState(jsonResult), is(SUCCESS));
                         BalanceDataTO balanceData = resultBody(jsonResult, BalanceDataTO.class);
                         assertThat(balanceData.getAddress(), nullValue());
-                        assertThat(balanceData.getLogisticPrice(), equalTo(LOGISTIC_PRICE));
+                        assertThat(balanceData.getLogisticPrice(), equalTo(LOGISTIC_PRICE_0));
                         assertThat(balanceData.getProductDistrict().getTotalNum(), equalTo(2));
-                        assertThat(balanceData.getProductDistrict().getTotalProductPrice(), equalTo(p1.getRealPrice() * 1 + p2.getRealPrice() * 1 + LOGISTIC_PRICE));
+                        assertThat(balanceData.getTotalPrice(), equalTo(p1.getRealPrice() * 1 + p2.getRealPrice() * 1 + LOGISTIC_PRICE_0));
                         assertThat(balanceData.getProductDistrict().getProductsImg()[0], equalTo(p1.getImage().split(";")[0]));
                     }
                 });
@@ -50,6 +49,7 @@ public class BalanceTest {
         final Product salePriceIs6P3 = ProductAcceptanceTestDataFactory.createSalePrice6Product3();
         String pids = salePriceIs6P3.getId().toString();
         String nums = "1";
+        final double LOGISTIC_PRICE_10 = 10d;
         anRequest(BALANCE_URL)
                 .withCookie(LoginController.USER_LOGIN_COOKIE, "MToxNDUxOTgyNjQzNTQ0OjM1ZWJhMDVjMjY5NTMxNjc5OWM1YmYwM2Q0YTE5N2M3")
                 .addParams("pids", pids)
@@ -59,9 +59,9 @@ public class BalanceTest {
                         assertThat(resultState(jsonResult), is(SUCCESS));
                         BalanceDataTO balanceData = resultBody(jsonResult, BalanceDataTO.class);
                         assertThat(balanceData.getAddress(), nullValue());
-                        assertThat(balanceData.getLogisticPrice(), equalTo(0d));
-                        assertThat(balanceData.getProductDistrict().getTotalNum(), equalTo(2));
-                        assertThat(balanceData.getProductDistrict().getTotalProductPrice(), equalTo(salePriceIs6P3.getRealPrice() * 1 + 0));
+                        assertThat(balanceData.getLogisticPrice(), equalTo(LOGISTIC_PRICE_10));
+                        assertThat(balanceData.getProductDistrict().getTotalNum(), equalTo(1));
+                        assertThat(balanceData.getTotalPrice(), equalTo(salePriceIs6P3.getRealPrice() * 1 + LOGISTIC_PRICE_10));
                         assertThat(balanceData.getProductDistrict().getProductsImg().length, equalTo(1));
                     }
                 });
